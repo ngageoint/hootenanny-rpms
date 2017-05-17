daaacbd7db677619b51760e4ac6f64e5fa7422d6
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2017-05-17 08:40:09 -0600

    Fixed indent
    1. refs #142

--- fix_postgres.sh
+++ fix_postgres.sh
@@ -8,17 +8,17 @@ if [ -n "$PG_VERSION" ]; then
         sudo -u postgres sed -i.bak s/^max_connections/\#max_connections/ /var/lib/pgsql/$PG_VERSION/data/postgresql.conf
         sudo -u postgres sed -i.bak s/^shared_buffers/\#shared_buffers/ /var/lib/pgsql/$PG_VERSION/data/postgresql.conf
         sudo -u postgres bash -c "cat >> /var/lib/pgsql/$PG_VERSION/data/postgresql.conf" <<EOT
-    #--------------
-    # Hoot Settings
-    #--------------
-    max_connections = 1000
-    shared_buffers = 1024MB
-    max_files_per_process = 1000
-    work_mem = 16MB
-    maintenance_work_mem = 256MB
-    checkpoint_segments = 20
-    autovacuum = off
-    EOT
+#--------------
+# Hoot Settings
+#--------------
+max_connections = 1000
+shared_buffers = 1024MB
+max_files_per_process = 1000
+work_mem = 16MB
+maintenance_work_mem = 256MB
+checkpoint_segments = 20
+autovacuum = off
+EOT
 
     sudo service postgresql-$PG_VERSION restart
     fi
