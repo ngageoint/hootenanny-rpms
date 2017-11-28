42e7a93bc2ad05c0f18733ed2ec602379ef804da
Justin Bronn <justin.bronn@digitalglobe.com>
2017-11-28 15:08:43 -0600

    Start remodeling the dependency build script to use Docker; remove old RPMs.

--- BuildDeps.sh
+++ BuildDeps.sh
@@ -1,153 +1,114 @@
 #!/bin/bash
-
-# Build RPM dependencies so we don't have to re-build them each time
-#
-# Yes, the versions are hardcoded. This can be fixed later.
-#
-
 set -e
-set -x
 
-cd /home/vagrant/hootenanny-rpms
+## Variables
 
-if  ! rpm -qa | grep --quiet FileGDBAPI; then
-    if [ ! -f el7-src/FileGDBAPI-1.5.1-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: FileGDBAPI"
-        pushd src
-        make -s RPMS/x86_64/FileGDBAPI-1.5.1-1.el7.x86_64.rpm
-        cp RPMS/x86_64/FileGDBAPI-1.5.1-1.el7.x86_64.rpm ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: FileGDBAPI"
-    sudo yum install -y el7-src/FileGDBAPI-1.5.1-1.el7.x86_64.rpm
+SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+GEOS_VERSION=$(cat $SCRIPT_HOME/src/SPECS/geos.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+GEOS_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/geos.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+GEOS_RPM=geos-$GEOS_VERSION-$GEOS_RELEASE.el7.x86_64.rpm
+GEOS_DEVEL_RPM=geos-devel-$GEOS_VERSION-$GEOS_RELEASE.el7.x86_64.rpm
+
+FILEGDBAPI_VERSION=$(cat $SCRIPT_HOME/src/SPECS/FileGDBAPI.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+FILEGDBAPI_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/FileGDBAPI.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+FILEGDBAPI_RPM=FileGDBAPI-$FILEGDBAPI_VERSION-$FILEGDBAPI_RELEASE.el7.x86_64.rpm
+
+LIBGEOTIFF_VERSION=$(cat $SCRIPT_HOME/src/SPECS/libgeotiff.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+LIBGEOTIFF_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/libgeotiff.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+LIBGEOTIFF_RPM=libgeotiff-$LIBGEOTIFF_VERSION-$LIBGEOTIFF_RELEASE.el7.x86_64.rpm
+LIBGEOTIFF_DEVEL_RPM=libgeotiff-devel-$LIBGEOTIFF_VERSION-$LIBGEOTIFF_RELEASE.el7.x86_64.rpm
+
+LIBKML_VERSION=$(cat $SCRIPT_HOME/src/SPECS/libkml.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+LIBKML_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/libkml.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+LIBKML_RPM=libkml-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
+LIBKML_DEVEL_RPM=libkml-devel-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
+
+# Important: Hootenanny and PostgreSQL depend on this.
+POSTGRES_VERSION=9.5
+
+## Build base images.
+
+docker build -t hoot/rpmbuild-base $SCRIPT_HOME/deps/base
+docker build -t hoot/rpmbuild-generic $SCRIPT_HOME/deps/generic
+docker build --build-arg pg_version=$POSTGRES_VERSION -t hoot/rpmbuild-pgdg$POSTGRES_VERSION $SCRIPT_HOME/deps/pgdg
+
+## Build GDAL dependencies.
+
+# FileGDBAPI
+if [ ! -f $SCRIPT_HOME/el7-src/$FILEGDBAPI_RPM ]; then
+    echo "#### Building RPM: FileGDBAPI"
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/FileGDBAPI.spec
+    cp $SCRIPT_HOME/src/RPMS/x86_64/$FILEGDBAPI_RPM $SCRIPT_HOME/el7-src
 fi
 
-if  ! rpm -qa | grep --quiet geos-3\.6\.1; then
-    if [ ! -f el7-src/geos-3.6.1-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: GEOS"
-        pushd src
-        make -s RPMS/x86_64/geos-3.6.1-1.el7.x86_64.rpm
-        cp  RPMS/x86_64/geos-* ../el7-src
-        rm ../el7-src/geos-debuginfo-3.6.1-1.el7.x86_64.rpm
-        popd
-    fi
-    echo "  Installing RPM: GEOS"
-    sudo yum install -y el7-src/geos-3.6.1-1.el7.x86_64.rpm
-    sudo yum install -y el7-src/geos-devel-3.6.1-1.el7.x86_64.rpm
+# GEOS
+if [ ! -f $SCRIPT_HOME/el7-src/$GEOS_RPM ]; then
+    echo "#### Building RPM: GEOS"
+
+    # Build image for building GEOS.
+    docker build \
+           --build-arg "packages=doxygen" \
+           -t hoot/rpmbuild-geos \
+           $SCRIPT_HOME/deps/generic
+
+    # Generate GEOS RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-geos \
+           rpmbuild -ba SPECS/geos.spec
+    cp $SCRIPT_HOME/src/RPMS/x86_64/{$GEOS_RPM,$GEOS_DEVEL_RPM} $SCRIPT_HOME/el7-src
 fi
 
-if  ! rpm -qa | grep --quiet libgeotiff-1\.4\.2; then
-    if [ ! -f el7-src/libgeotiff-1.4.2-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: libgeotiff"
-        pushd src
-        make -s RPMS/x86_64/libgeotiff-1.4.2-1.el7.x86_64.rpm
-        cp  RPMS/x86_64/libgeotiff-* ../el7-src
-        rm ../el7-src/libgeotiff-debuginfo-1.4.2-1.el7.x86_64.rpm
-        popd
-    fi
-    echo "  Installing RPM: libgeotiff"
-    sudo yum install -y el7-src/libgeotiff-1.4.2-1.el7.x86_64.rpm
-    sudo yum install -y el7-src/libgeotiff-devel-1.4.2-1.el7.x86_64.rpm
+# libgeotiff
+if [ ! -f $SCRIPT_HOME/el7-src/$LIBGEOTIFF_RPM ]; then
+    echo "#### Building RPM: libgeotiff"
+    LIBGEOTIFF_REQUIRES=$(cat $SCRIPT_HOME/src/SPECS/libgeotiff.spec | grep '^BuildRequires:' | awk '{ for (i = 2; i <= NF; ++i) if ($i ~ /^[[:alpha:]]/ && $i !~ /[\%\{\}]/) print $i }' ORS=" ")
+
+    # Build image for building libgeotiff.
+    docker build \
+           --build-arg "packages=${LIBGEOTIFF_REQUIRES}" \
+           -t hoot/rpmbuild-libgeotiff \
+           $SCRIPT_HOME/deps/generic
+
+    # Generate libgeotiff RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-libgeotiff \
+           rpmbuild -ba SPECS/libgeotiff.spec
+    cp $SCRIPT_HOME/src/RPMS/x86_64/{$LIBGEOTIFF_RPM,$LIBGEOTIFF_DEVEL_RPM} $SCRIPT_HOME/el7-src
 fi
 
-if  ! rpm -qa | grep --quiet libkml-1\.3\.0; then
-    if [ ! -f el7-src/libkml-1.3.0-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: libkml"
-        pushd src
-        make -s RPMS/x86_64/libkml-1.3.0-1.el7.x86_64.rpm
-        cp  RPMS/x86_64/libkml-* ../el7-src
-        rm ../el7-src/libkml-debuginfo-1.3.0-1.el7.x86_64.rpm
-        popd
-    fi
-    echo "  Installing RPM: libkml"
-    sudo yum install -y el7-src/libkml-1.3.0-1.el7.x86_64.rpm
-    sudo yum install -y el7-src/libkml-devel-1.3.0-1.el7.x86_64.rpm
-fi
+# libkml
+if [ ! -f $SCRIPT_HOME/el7-src/$LIBKML_RPM ]; then
+    echo "#### Building RPM: libkml"
+    LIBKML_REQUIRES=$(cat $SCRIPT_HOME/src/SPECS/libkml.spec | grep '^BuildRequires:' | awk '{ for (i = 2; i <= NF; ++i) if ($i ~ /^[[:alpha:]]/ && $i !~ /[\%\{\}]/) print $i }' ORS=" ")
 
-if  ! rpm -qa | grep --quiet stxxl-1.3.1; then
-    if [ ! -f el7-src/stxxl-1.3.1-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: stxxl"
-        pushd src
-        make -s RPMS/x86_64/stxxl-1.3.1-1.el7.x86_64.rpm
-        cp RPMS/x86_64/stxxl-1.3.1-1.el7.x86_64.rpm ../el7-src
-        cp RPMS/x86_64/stxxl-devel-1.3.1-1.el7.x86_64.rpm ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: stxxl"
-    sudo yum install -y el7-src/stxxl-1.3.1-1.el7.x86_64.rpm el7-src/stxxl-devel-1.3.1-1.el7.x86_64.rpm
-fi
+    # Build image for building libkml.
+    docker build \
+           --build-arg "packages=${LIBKML_REQUIRES}" \
+           -t hoot/rpmbuild-libkml \
+           $SCRIPT_HOME/deps/generic
 
-if  ! rpm -qa | grep --quiet hoot-words; then
-    if [ ! -f el7-src/hoot-words-1.0.0-1.el7.noarch.rpm ]; then
-        echo "#### Building RPM: Hootenanny Words"
-        pushd src
-        make -s RPMS/noarch/hoot-words-1.0.0-1.el7.noarch.rpm
-        cp RPMS/noarch/hoot-words-1.0.0-1.el7.noarch.rpm  ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: Hootenanny Words"
-    sudo yum install -y el7-src/hoot-words-1.0.0-1.el7.noarch.rpm
-fi
-
-if  ! rpm -qa | grep --quiet wamerican-insane; then
-    if [ ! -f el7-src/wamerican-insane-7.1-1.el7.noarch.rpm ]; then
-        echo "#### Building RPM: American Insane Dictionary"
-        pushd src
-        make -s RPMS/noarch/wamerican-insane-7.1-1.el7.noarch.rpm
-        cp RPMS/noarch/wamerican-insane-7.1-1.el7.noarch.rpm  ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: American Insane Dictionary"
-    sudo yum install -y el7-src/wamerican-insane-7.1-1.el7.noarch.rpm
-fi
-
-if  ! rpm -qa | grep --quiet osmosis; then
-    if [ ! -f el7-src/osmosis-0.46-1.el7.noarch.rpm ]; then
-        echo "#### Building RPM: Osmosis"
-        pushd src
-        make -s RPMS/noarch/osmosis-0.46-1.el7.noarch.rpm
-        cp RPMS/noarch/osmosis-0.46-1.el7.noarch.rpm  ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: Osmosis"
-    sudo yum install -y el7-src/osmosis-0.46-1.el7.noarch.rpm
-fi
-
-if  ! rpm -qa | grep --quiet tomcat8; then
-    if [ ! -f el7-src/tomcat8-8.5.23-1.el7.noarch.rpm ]; then
-        echo "#### Building RPM: Tomcat8"
-        pushd src
-        make -s RPMS/noarch/tomcat8-8.5.23-1.el7.noarch.rpm
-        cp  RPMS/noarch/tomcat8-8.5.23-1.el7.noarch.rpm ../el7-src
-        popd
-    fi
-    echo "  Installing RPM: Tomcat8"
-    sudo yum install -y el7-src/tomcat8-8.5.23-1.el7.noarch.rpm
-fi
-
-if  ! rpm -qa | grep --quiet hoot-gdal; then
-    if [ ! -f el7-src/hoot-gdal-2.1.4-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: GDAL"
-        pushd src
-        make -s RPMS/x86_64/hoot-gdal-2.1.4-1.el7.x86_64.rpm
-        cp  RPMS/x86_64/hoot-gdal-* ../el7-src
-        # Save space
-        rm ../el7-src/hoot-gdal-debuginfo-2.1.4-1.el7.x86_64.rpm
-        popd
-    fi
-    echo "  Installing RPM: GDAL"
-    sudo yum install -y el7-src/hoot-gdal-*
-fi
-
-if  ! rpm -qa | grep --quiet hoot-postgis; then
-    if [ ! -f el7-src/hoot-postgis23_95-2.3.4-1.el7.x86_64.rpm ]; then
-        echo "#### Building RPM: hoot-postgis"
-        pushd src
-        make -s RPMS/x86_64/hoot-postgis23_95-2.3.4-1.el7.x86_64.rpm
-        cp  RPMS/x86_64/hoot-postgis23_95-* ../el7-src
-        rm ../el7-src/hoot-postgis23_95-debuginfo-2.3.4-1.el7.x86_64.rpm
-        popd
-    fi
-    echo "  Installing RPM: PostGIS"
-    sudo yum install -y el7-src/hoot-postgis23_95-*
+    # Generate libkml RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-libkml \
+           rpmbuild -ba SPECS/libkml.spec
+    cp $SCRIPT_HOME/src/RPMS/x86_64/{$LIBKML_RPM,$LIBKML_DEVEL_RPM} $SCRIPT_HOME/el7-src
 fi
