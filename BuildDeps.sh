a842b1ea3f42a7e907c1db20c37553294d5378fc
Justin Bronn <justin.bronn@digitalglobe.com>
2017-11-28 16:15:07 -0600

    Finish non-GDAL dependencies.

--- BuildDeps.sh
+++ BuildDeps.sh
@@ -24,6 +24,28 @@ LIBKML_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/libkml.spec | grep '^Release:' | awk
 LIBKML_RPM=libkml-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
 LIBKML_DEVEL_RPM=libkml-devel-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
 
+OSMOSIS_VERSION=$(cat $SCRIPT_HOME/src/SPECS/osmosis.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+OSMOSIS_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/osmosis.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+OSMOSIS_RPM=osmosis-$OSMOSIS_VERSION-$OSMOSIS_RELEASE.el7.noarch.rpm
+
+STXXL_VERSION=$(cat $SCRIPT_HOME/src/SPECS/stxxl.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+STXXL_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/stxxl.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+STXXL_RPM=stxxl-$STXXL_VERSION-$STXXL_RELEASE.el7.x86_64.rpm
+STXXL_DEVEL_RPM=stxxl-devel-$STXXL_VERSION-$STXXL_RELEASE.el7.x86_64.rpm
+
+TOMCAT8_VERSION=$(cat $SCRIPT_HOME/src/SPECS/tomcat8.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+TOMCAT8_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/tomcat8.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+TOMCAT8_RPM=tomcat8-$TOMCAT8_VERSION-$TOMCAT8_RELEASE.el7.noarch.rpm
+
+WAMERICAN_VERSION=$(cat $SCRIPT_HOME/src/SPECS/wamerican-insane.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+WAMERICAN_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/wamerican-insane.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+WAMERICAN_RPM=wamerican-insane-$WAMERICAN_VERSION-$WAMERICAN_RELEASE.el7.noarch.rpm
+
+WORDS_VERSION=$(cat $SCRIPT_HOME/src/SPECS/hoot-words.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }')
+WORDS_RELEASE=$(cat $SCRIPT_HOME/src/SPECS/hoot-words.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }')
+WORDS_RPM=hoot-words-$WORDS_VERSION-$WORDS_RELEASE.el7.noarch.rpm
+
+
 # Important: Hootenanny and PostgreSQL depend on this.
 POSTGRES_VERSION=9.5
 
@@ -111,4 +133,82 @@ if [ ! -f $SCRIPT_HOME/el7-src/$LIBKML_RPM ]; then
            hoot/rpmbuild-libkml \
            rpmbuild -ba SPECS/libkml.spec
     cp $SCRIPT_HOME/src/RPMS/x86_64/{$LIBKML_RPM,$LIBKML_DEVEL_RPM} $SCRIPT_HOME/el7-src
+fi
+
+
+## Simple Dependencies
+
+# hoot-words
+if [ ! -f $SCRIPT_HOME/el7-src/$WORDS_RPM ]; then
+    echo "#### Building RPM: hoot-words"
+
+    # Generate hoot-words RPM (do not share SOURCES directory, as
+    # it's forced download from spec file).
+    docker run \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/hoot-words.spec
+    cp $SCRIPT_HOME/src/RPMS/noarch/$WORDS_RPM $SCRIPT_HOME/el7-src
+fi
+
+# osmosis
+if [ ! -f $SCRIPT_HOME/el7-src/$OSMOSIS_RPM ]; then
+    echo "#### Building RPM: osmosis"
+
+    # Generate osmosis RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/osmosis.spec
+    cp $SCRIPT_HOME/src/RPMS/noarch/$OSMOSIS_RPM $SCRIPT_HOME/el7-src
+fi
+
+# stxxl
+if [ ! -f $SCRIPT_HOME/el7-src/$STXXL_RPM ]; then
+    echo "#### Building RPM: stxxl"
+
+    # Generate stxxl RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/stxxl.spec
+    cp $SCRIPT_HOME/src/RPMS/x86_64/{$STXXL_RPM,$STXXL_DEVEL_RPM} $SCRIPT_HOME/el7-src
+fi
+
+# tomcat8
+if [ ! -f $SCRIPT_HOME/el7-src/$TOMCAT8_RPM ]; then
+    echo "#### Building RPM: tomcat8"
+
+    # Generate tomcat8 RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/tomcat8.spec
+    cp $SCRIPT_HOME/src/RPMS/noarch/$TOMCAT8_RPM $SCRIPT_HOME/el7-src
+fi
+
+# wamerican-insane
+if [ ! -f $SCRIPT_HOME/el7-src/$WAMERICAN_RPM ]; then
+    echo "#### Building RPM: wamerican-insane"
+
+    # Generate wamerican-insane RPM.
+    docker run \
+           -v "${SCRIPT_HOME}/src/SOURCES":/rpmbuild/SOURCES:ro \
+           -v "${SCRIPT_HOME}/src/SPECS":/rpmbuild/SPECS:ro \
+           -v "${SCRIPT_HOME}/src/RPMS":/rpmbuild/RPMS:rw \
+           -it --rm \
+           hoot/rpmbuild-generic \
+           rpmbuild -ba SPECS/wamerican-insane.spec
+    cp $SCRIPT_HOME/src/RPMS/noarch/$WAMERICAN_RPM $SCRIPT_HOME/el7-src
 fi
