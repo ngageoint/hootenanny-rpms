2a90167c0d0cccf42dbb8b3d221986021ff1a784
Justin Bronn <justin.bronn@digitalglobe.com>
2017-12-12 17:01:48 -0600

    Move variables/functions to `Vars.sh`.

--- BuildDeps.sh
+++ BuildDeps.sh
@@ -1,92 +1,16 @@
 #!/bin/bash
 set -e
 
-## Script location and utility functions.
-
+## Get variables.
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Get version from spec file.
-function get_version() {
-    cat $SCRIPT_HOME/src/SPECS/$1.spec | grep '^Version:[[:space:]]\+[[:digit:]]' | awk '{ print $2 }'
-}
-
-# Get release number from spec file.
-function get_release() {
-    cat $SCRIPT_HOME/src/SPECS/$1.spec | grep '^Release:' | awk '{ gsub(/[^0-9]/, "", $2); print $2 }'
-}
-
-# Get build requirement packages from spec file.
-function get_requires() {
-    # Note: This does not respect `BuildRequires` statements within conditional
-    #       macro statements nor version specifiers.
-    cat $SCRIPT_HOME/src/SPECS/$1.spec | grep '^BuildRequires:' | awk '{ for (i = 2; i <= NF; ++i) if ($i ~ /^[[:alpha:]]/ && $i !~ /[\%\{\}]/) print $i }' ORS=' '
-}
-
-
-## Package versioning variables.
-
-# Where binary RPMs are placed.
-RPM_X86_64=$SCRIPT_HOME/src/RPMS/x86_64
-RPM_NOARCH=$SCRIPT_HOME/src/RPMS/noarch
-
-# Important: Hootenanny and PostgreSQL depend on this.
-PG_VERSION=9.5
-PG_DOTLESS=$(echo $PG_VERSION | tr -d '.')
-
-FILEGDBAPI_VERSION=$( get_version FileGDBAPI )
-FILEGDBAPI_RELEASE=$( get_release FileGDBAPI )
-FILEGDBAPI_RPM=FileGDBAPI-$FILEGDBAPI_VERSION-$FILEGDBAPI_RELEASE.el7.x86_64.rpm
-
-GDAL_VERSION=$( get_version hoot-gdal )
-GDAL_RELEASE=$( get_release hoot-gdal )
-GDAL_RPM_SUFFIX=$GDAL_VERSION-$GDAL_RELEASE.el7.x86_64.rpm
-GDAL_RPM=hoot-gdal-$GDAL_RPM_SUFFIX
-
-GEOS_VERSION=$( get_version geos )
-GEOS_RELEASE=$( get_release geos )
-GEOS_RPM=geos-$GEOS_VERSION-$GEOS_RELEASE.el7.x86_64.rpm
-GEOS_DEVEL_RPM=geos-devel-$GEOS_VERSION-$GEOS_RELEASE.el7.x86_64.rpm
-
-LIBGEOTIFF_VERSION=$( get_version libgeotiff )
-LIBGEOTIFF_RELEASE=$( get_release libgeotiff )
-LIBGEOTIFF_RPM=libgeotiff-$LIBGEOTIFF_VERSION-$LIBGEOTIFF_RELEASE.el7.x86_64.rpm
-LIBGEOTIFF_DEVEL_RPM=libgeotiff-devel-$LIBGEOTIFF_VERSION-$LIBGEOTIFF_RELEASE.el7.x86_64.rpm
-
-LIBKML_VERSION=$( get_version libkml )
-LIBKML_RELEASE=$( get_release libkml )
-LIBKML_RPM=libkml-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
-LIBKML_DEVEL_RPM=libkml-devel-$LIBKML_VERSION-$LIBKML_RELEASE.el7.x86_64.rpm
-
-OSMOSIS_VERSION=$( get_version osmosis )
-OSMOSIS_RELEASE=$( get_release osmosis )
-OSMOSIS_RPM=osmosis-$OSMOSIS_VERSION-$OSMOSIS_RELEASE.el7.noarch.rpm
-
-POSTGIS_VERSION=$( get_version hoot-postgis23 )
-POSTGIS_RELEASE=$( get_release hoot-postgis23 )
-POSTGIS_RPM=hoot-postgis23_$PG_DOTLESS-$POSTGIS_VERSION-$POSTGIS_RELEASE.el7.x86_64.rpm
-
-STXXL_VERSION=$( get_version stxxl )
-STXXL_RELEASE=$( get_release stxxl )
-STXXL_RPM=stxxl-$STXXL_VERSION-$STXXL_RELEASE.el7.x86_64.rpm
-STXXL_DEVEL_RPM=stxxl-devel-$STXXL_VERSION-$STXXL_RELEASE.el7.x86_64.rpm
-
-TOMCAT8_VERSION=$( get_version tomcat8 )
-TOMCAT8_RELEASE=$( get_release tomcat8 )
-TOMCAT8_RPM=tomcat8-$TOMCAT8_VERSION-$TOMCAT8_RELEASE.el7.noarch.rpm
-
-WAMERICAN_VERSION=$( get_version wamerican-insane )
-WAMERICAN_RELEASE=$( get_release wamerican-insane )
-WAMERICAN_RPM=wamerican-insane-$WAMERICAN_VERSION-$WAMERICAN_RELEASE.el7.noarch.rpm
-
-WORDS_VERSION=$( get_version hoot-words )
-WORDS_RELEASE=$( get_release hoot-words )
-WORDS_RPM=hoot-words-$WORDS_VERSION-$WORDS_RELEASE.el7.noarch.rpm
+source $SCRIPT_HOME/Vars.sh
 
 ## Build base images.
 
 # Base image that has basic development, RPM authoring, and
 # a non-privileged user for building spec files.
 docker build \
+       --build-arg rpmbuild_dist=$RPMBUILD_DIST \
        --build-arg rpmbuild_uid=$(id -u) \
        -f $SCRIPT_HOME/docker/base/Dockerfile \
        -t hoot/rpmbuild-base \
