47e41b166729864a32d4f981c965b9bd5a6cd46e
Justin Bronn <justin.bronn@digitalglobe.com>
2018-01-11 09:30:58 -0600

    Use `m2` and `npm` directories relative to repository directory as caches for maven and npm, respectively.

--- BuildArchive.sh
+++ BuildArchive.sh
@@ -8,15 +8,18 @@ source $SCRIPT_HOME/Vars.sh
 # The container used to build Hootenanny archive.
 BUILD_IMAGE="${BUILD_IMAGE:-hoot/rpmbuild-hoot-release}"
 
-mkdir -p $SCRIPT_HOME/hootenanny $SCRIPT_HOME/m2
+mkdir -p \
+      $SCRIPT_HOME/hootenanny \
+      $SCRIPT_HOME/m2 \
+      $SCRIPT_HOME/npm
 
 docker run \
        -v $SOURCES:/rpmbuild/SOURCES:rw \
        -v $SPECS:/rpmbuild/SPECS:ro \
-       -v $RPMS:/rpmbuild/RPMS:rw \
        -v $SCRIPT_HOME/scripts:/rpmbuild/scripts:ro \
        -v $SCRIPT_HOME/hootenanny:/rpmbuild/hootenanny:rw \
        -v $SCRIPT_HOME/m2:/rpmbuild/.m2:rw \
+       -v $SCRIPT_HOME/npm:/rpmbuild/.npm:rw \
        -it --rm \
        $BUILD_IMAGE \
        /bin/bash -c '/rpmbuild/scripts/hoot-checkout.sh && /rpmbuild/scripts/hoot-archive.sh'
