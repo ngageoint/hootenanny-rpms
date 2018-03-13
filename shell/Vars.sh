ad836e05fa6868051e0534b4eb8faf498485f2d5
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-03-13 15:26:39 -0500

    Use `sha1sum`, as `shasum` is a CentOS-only construct.

--- shell/Vars.sh
+++ shell/Vars.sh
@@ -193,7 +193,7 @@ function build_run_images() {
 function maven_cache() {
     if ! test -d $CACHE/m2/repository; then
         curl -sSL -o /var/tmp/m2-cache.tar.gz $MAVEN_CACHE_URL
-        echo "${MAVEN_CACHE_SHA1}  /var/tmp/m2-cache.tar.gz" | shasum -c -
+        echo "${MAVEN_CACHE_SHA1}  /var/tmp/m2-cache.tar.gz" | sha1sum -c -
         tar -C $CACHE/m2 -xzf /var/tmp/m2-cache.tar.gz
     fi
 }
