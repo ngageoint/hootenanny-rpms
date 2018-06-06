b29ec9223c282cadcfb98952f809e10b734329eb
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-06-06 16:08:52 -0500

    * Simplify, since only being run once in a container. * Use `su-exec` to invoke `curl` and `unzip`.

--- scripts/sonar-install.sh
+++ scripts/sonar-install.sh
@@ -14,36 +14,31 @@ SONAR_BLD_PKG=build-wrapper-linux-x86
 SONAR_BLD_ZIP=$SONAR_BLD_PKG.zip
 SONAR_BLD_URL=https://sonarcloud.io/static/cpp
 
-# Download sonar scanner
-if [ ! -f $SONAR_ZIP ]; then
-    echo "### Downloading Sonar Scanner..."
-    curl -sSL -O $SONAR_URL/$SONAR_ZIP
-fi
+# Do everything in temporary folder.
+pushd /var/tmp
 
-# Install sonar scanner
-if [ ! -f $SONAR_PATH ]; then
-    echo "### Installing Sonar Scanner..."
-    if [ ! -f $SONAR_PKG ]; then
-        unzip -qq -o $SONAR_ZIP
-    fi
-    mv $SONAR_PKG $SONAR_PATH
-    echo "pathmunge $SONAR_PATH/bin after" > /etc/profile.d/sonarqube.sh
-fi
+echo "### Downloading Sonar Scanner..."
+su-exec $RPMBUILD_USER curl -sSL -O $SONAR_URL/$SONAR_ZIP
 
-# Download sonar build wrapper
-if [ ! -f $SONAR_BLD_ZIP ]; then
-    echo "### Downloading Sonar Build Wrapper..."
-    curl -sSL -O $SONAR_BLD_URL/$SONAR_BLD_ZIP
-fi
+echo "### Installing Sonar Scanner..."
+su-exec $RPMBUILD_USER unzip -qq -o $SONAR_ZIP
+chown -R root:root $SONAR_PKG
+mv $SONAR_PKG $SONAR_PATH
 
-#Install sonar build wrapper
-if [ ! -f $SONAR_PATH/bin/$SONAR_BLD_PKG-64 ]; then
-    echo "### Installing Sonar Build Wrapper..."
-    if [ ! -f $SONAR_BLD_PKG ]; then
-        unzip -qq -o $SONAR_BLD_ZIP
-    fi
-    mv $SONAR_BLD_PKG/$SONAR_BLD_PKG-64 $SONAR_PATH/bin/
-    mv $SONAR_BLD_PKG/libinterceptor-x86_64.so $SONAR_PATH/bin/
-    mv $SONAR_BLD_PKG/libinterceptor-i686.so $SONAR_PATH/bin/
-    rm -rf $SONAR_BLD_PKG
-fi
+# Add the sonar binaries to the system profile $PATH.
+echo "pathmunge $SONAR_PATH/bin after" > /etc/profile.d/sonarqube.sh
+
+echo "### Downloading Sonar Build Wrapper..."
+su-exec $RPMBUILD_USER curl -sSL -O $SONAR_BLD_URL/$SONAR_BLD_ZIP
+
+echo "### Installing Sonar Build Wrapper..."
+su-exec $RPMBUILD_USER unzip -qq -o $SONAR_BLD_ZIP
+chown -R root:root $SONAR_BLD_PKG
+mv $SONAR_BLD_PKG/$SONAR_BLD_PKG-64 $SONAR_PATH/bin/
+mv $SONAR_BLD_PKG/libinterceptor-x86_64.so $SONAR_PATH/bin/
+mv $SONAR_BLD_PKG/libinterceptor-i686.so $SONAR_PATH/bin/
+
+echo "### Cleaning Up..."
+rm -rf $SONAR_BLD_PKG $SONAR_ZIP $SONAR_BLD_ZIP
+
+popd
