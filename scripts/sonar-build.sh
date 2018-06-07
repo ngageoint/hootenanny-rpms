5719d7f0df0c0cdb105af87281eccecdfd8ec146
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-06-07 10:21:28 -0500

    Remove UI configuration.

--- scripts/sonar-build.sh
+++ scripts/sonar-build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Setup hoot's autoconf files.
 aclocal && autoconf && autoheader && automake --add-missing --copy
-./configure --quiet --with-rnd --with-services --with-uitests --with-coverage
+./configure --quiet --with-rnd --with-services --with-coverage
 
 # Make with the sonar build watcher
 build-wrapper-linux-x86-64 --out-dir bw-output make -sj"$(nproc)"
