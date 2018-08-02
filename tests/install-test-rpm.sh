69bc3adac9eb7f8d0875b30ce883670834ee684c
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-08-02 12:48:48 -0500

    Remove debugging configuration.

--- tests/install-test-rpm.sh
+++ tests/install-test-rpm.sh
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-set -euxo pipefail
+set -euo pipefail
 
 HOOT_TESTS="${HOOT_TESTS:-yes}"
 HOOT_TESTS_PARALLEL="${HOOT_TESTS_PARALLEL:-$(nproc)}"
