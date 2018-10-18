20df81668d4251ee1e1f37aa7ebc6a6e21c3dee3
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-10-18 10:42:30 -0500

    Upgrade to Vagrant 2.2.0.

--- scripts/vagrant-install.sh
+++ scripts/vagrant-install.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 #  * VAGRANT_VERSION
 #  * VAGRANT_BASEURL
 LSB_DIST="$(. /etc/os-release && echo "$ID")"
-VAGRANT_VERSION="${VAGRANT_VERSION:-2.1.4}"
+VAGRANT_VERSION="${VAGRANT_VERSION:-2.2.0}"
 VAGRANT_BASEURL="${VAGRANT_BASEURL:-https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION}"
 
 # Set up command differences between RHEL and Debian-based systems.
