087423a5f116817c1cd2fce0a3f9438ead7a75bd
Garret Voltz <garret.voltz@radiantsolutions.com>
2019-03-14 15:10:01 -0400

    update to exit out when a previous version of the hoot rpm can't be found

--- scripts/install-previous-hoot-version.sh
+++ scripts/install-previous-hoot-version.sh
@@ -6,6 +6,13 @@ PREVIOUS_VERSION=`sudo yum list --showduplicates hootenanny-autostart | \
                       head -1 | \
                       awk '{ print $2 }'`
 
+# if the previous version couldn't be discovered in the hoot yum repo
+# then print an error message and exit 
+if [ -z ${PREVIOUS_VERSION} ]; then
+    echo "Failed to discover the previous RPM version of Hootenanny"
+    exit 1
+fi
+
 INSTALLED_HOOT_VERSION=`rpm -qa | grep ${HOOT_RPM_PREFIX}`
 # if a version of hoot is already install downgrade it
 # else install the 2nd latest version of hootenanny
