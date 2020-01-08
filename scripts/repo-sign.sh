e4dc9ddd53dc1ff7797d24df7b97f4afc86c3f6a
Justin Bronn <jbronn@gmail.com>
2020-01-08 12:39:11 -0600

    Simplify shell script logic and variables.

--- scripts/repo-sign.sh
+++ scripts/repo-sign.sh
@@ -15,16 +15,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 set -euo pipefail
 
-set +u
-if [ -z $1 ]; then
+REPO="${1:-}"
+if [ -z "$REPO" ]; then
     echo 'repo-sign.sh: must provide a repository directory argument.'
     exit 1
 fi
-set -u
-
-REPO=$1
-REPODATA=$REPO/repodata
-REPOMD=$REPODATA/repomd.xml
 
 # Sign the repository metadata file.
-gpg --detach-sign --armor $REPOMD
+gpg --detach-sign --armor "$REPO/repodata/repomd.xml"
