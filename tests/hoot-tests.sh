6461c56c3339dc10e6de881f5e0de0dcd3b134a6
Brian Hatchl <brian.hatchl@maxar.com>
2022-04-20 23:32:04 -0400

    just run quick tests on test rpm install

--- tests/hoot-tests.sh
+++ tests/hoot-tests.sh
@@ -17,20 +17,20 @@ set -euo pipefail
 
 HOOT_TESTS="${HOOT_TESTS:-yes}"
 HOOT_TESTS_PARALLEL="${HOOT_TESTS_PARALLEL:-$(nproc)}"
-HOOT_TESTS_OPT="${HOOT_TESTS_OPT:---glacial}"
+HOOT_TESTS_OPT="${HOOT_TESTS_OPT:---quick}"
 
 if [ "$HOOT_TESTS" = "yes" -a ! -f el7/none.rpm ]; then
     # Setup database for testing.
     ./scripts/postgresql-install.sh
     ./scripts/hoot-db-setup.sh
     su-exec postgres pg_ctl -D "$PGDATA" -s start
-    
+
     # Start Tomcat and node-export and send output to logfiles.
     touch /var/log/{node-export,tomcat8}.log
     chown tomcat:tomcat /var/log/{node-export,tomcat8}.log
     su-exec tomcat /usr/libexec/tomcat8/server start &> /var/log/tomcat8.log &
     su-exec tomcat bash -c "cd /var/lib/hootenanny/node-export-server && npm start" &> /var/log/node-export.log &
-    
+
     # Run Hootenanny tests.
     cd /var/lib/hootenanny
     su-exec tomcat HootTest --diff "$HOOT_TESTS_OPT" --parallel "$HOOT_TESTS_PARALLEL"
